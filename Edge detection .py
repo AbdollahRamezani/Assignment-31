291eba49618aa11ee1afd94a87f7d10ecae4f1ba
Abdollah Ramezani <53383194+ramezani84@users.noreply.github.com>
2023-04-29 16:49:05 +0330

    Update Edge detection .py

--- Edge detection .py
+++ Edge detection .py
@@ -11,7 +11,7 @@ filter = np.array([[-1, -1, -1],
 
 for i in range(1, rows-1):
     for j in range(1, cols-1):
-        small = image[i-1:i+2, j-1:j+2]  #چون کران بالا را درنظر نمیگیرد از i+2وj+2 استفاده شده
+        small = image[i-1:i+2, j-1:j+2]  
         average = np.abs(np.sum(filter * small))
         result[i, j] = average
 
